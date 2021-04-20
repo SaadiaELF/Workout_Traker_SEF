ac87a095d3c8b77648a19a24d022ffbd255b6bff
Saadia El fekak <saadiaelfekak@gmail.com>
2021-04-20 13:48:15 +0100

    Adding get route to exercise page

---
+++ Develop/routes/homeRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const Workout = require("./../models/workout.js");
+const path = require('path');
+
+
+router.get("/exercise", (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, '../public/exercise.html'));
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
+
+
+module.exports = router;

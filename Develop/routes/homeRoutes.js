a4af27ac0a3689dc3d27f2fe089920d714b9904f
Saadia El fekak <saadiaelfekak@gmail.com>
2021-04-21 12:00:10 +0100

    Adding get stats route

--- Develop/routes/homeRoutes.js
+++ Develop/routes/homeRoutes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const Workout = require("./../models/workout.js");
 const path = require('path');
 
-
 router.get("/exercise", (req, res) => {
     try {
         res.sendFile(path.join(__dirname, '../public/exercise.html'));
@@ -11,5 +10,12 @@ router.get("/exercise", (req, res) => {
     }
 });
 
+router.get("/stats", (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, '../public/stats.html'));
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
 
 module.exports = router;

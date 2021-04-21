f4cff458263ac7094e12b4669fb9301ec727cff4
Saadia El fekak <saadiaelfekak@gmail.com>
2021-04-21 18:14:40 +0100

    Adding get range route

--- Develop/routes/api/workoutRoutes.js
+++ Develop/routes/api/workoutRoutes.js
@@ -32,4 +32,14 @@ router.get("/", (req, res) => {
         });
 });
 
+router.get("/range", (req, res) => {
+    Workout.find()
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
 module.exports = router;
